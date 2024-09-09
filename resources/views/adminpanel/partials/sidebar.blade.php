c48756f6ecbc40013dc3fb265e0a362c5265b9c1
kayumazaman <kayumazaman222@gmail.com>
2024-09-09 07:35:30 +0600

    changed some fornt end UI

--- resources/views/adminpanel/partials/sidebar.blade.php
+++ resources/views/adminpanel/partials/sidebar.blade.php
@@ -25,8 +25,8 @@
         </ul><span class="heading">Extras</span>
         <ul class="list-unstyled">
           <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
-          <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
-          <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>
+          <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo 1</a></li>
+          <li> <a href="#"> <i class="icon-chart"></i>Demo 2 </a></li>
         </ul>
       </nav>
       <!-- Sidebar Navigation end-->
